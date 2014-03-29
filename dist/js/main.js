d59cf8602c6648801e8e25f4326313d36a698657
Pieter Van Molle <pietervanmolle@Pieters-MacBook-Air.local>
2014-03-29 17:08:11 +0100

    Profile page

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,23 +4,40 @@ google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Day', 'Donated'],
-        ['Mon',   1.0    ],
-        ['Tue',   0.0    ],
-        ['Wen',   2.3    ],
-        ['Thu',   0.4    ],
-        ['Fri',   2.0    ],
-        ['Sat',   0.0    ],
-        ['Sun',   0.0    ]
+        ['Mon', 1.0    ],
+        ['Tue', 0.0    ],
+        ['Wen', 2.3    ],
+        ['Thu', 0.4    ],
+        ['Fri', 2.0    ],
+        ['Sat', 0.0    ],
+        ['Sun', 0.0    ]
     ]);
 
-    new google.visualization.ColumnChart(document.getElementById('chart')).
-        draw(data, {
-            width: $('#chart').width(),
-            height: 200,
-            colors: ['#ff6600'],
-            legend: { position: 'none' },
-            chartArea: {
-                width: '100%'
-            }
-        });
+    var chartDonator;
+    if ((chartDonator = document.getElementById('chart-donator')) !== null) {
+        new google.visualization.ColumnChart(chartDonator).
+            draw(data, {
+                width: $('#chart-donator').width(),
+                height: 200,
+                colors: ['#ff6600'],
+                legend: { position: 'none' },
+                chartArea: {
+                    width: '100%'
+                }
+            });
+    }
+
+    var chartRecipient;
+    if ((chartRecipient = document.getElementById('chart-recipient')) !== null) {
+        new google.visualization.LineChart(chartRecipient).
+            draw(data, {
+                width: $('#chart-recipient').width(),
+                height: 200,
+                colors: ['#ff6600'],
+                legend: { position: 'none' },
+                chartArea: {
+                    width: '100%'
+                }
+            });
+    }
 }
